df8fbc6598912058a1202775e8fc0475940a3099
Aleksandrs Levi <aleksandrslevi@gmail.com>
2017-03-17 18:05:10 +0100

    added more test cases

--- ChangeCalculator/src/TestChangeCalculator.java
+++ ChangeCalculator/src/TestChangeCalculator.java
@@ -11,11 +11,17 @@ public class TestChangeCalculator {
     @Test
     public void testMakeChange(){
         ChangeCalculator changeCalculator = new ChangeCalculator(); //testing changeCalculator
-        assertEquals(4, changeCalculator.makeChange(135));
+        assertEquals(0, changeCalculator.makeChange(0));
+        assertEquals(4, changeCalculator.makeChange(4));
+        assertEquals(5, changeCalculator.makeChange(18));
         assertEquals(3, changeCalculator.makeChange(71));
         assertEquals(3, changeCalculator.makeChange(111));
+        assertEquals(4, changeCalculator.makeChange(135));
+        assertEquals(7 ,changeCalculator.makeChange(323));
         assertEquals(24, changeCalculator.makeChange(2027));
         assertEquals(102, changeCalculator.makeChange(10006));
+        assertEquals(214 ,changeCalculator.makeChange(21053));
+
     }
 
     @Test (expected = NumberFormatException.class)
